7cb4b298b7e00d35ccdbe2c8ff15e099247975fa
ze-fernando <jfenginner935@gmail.com>
2023-09-27 20:17:13 -0300

    Create class

---
+++ hotelReservation/models/Pessoa.cs
@@ -0,0 +1,4 @@
+public class Pessoa{
+    public string nome {get; set;}
+    public string sobrenome {get; set;}
+}
---
+++ hotelReservation/models/Suite.cs
@@ -0,0 +1,5 @@
+public class suite{
+    public string tipoSuite {get; set;}
+    public int capacidade {get; set;}
+    public decimal valorDiaria {get; set;}
+}
